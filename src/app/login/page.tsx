8e71f548d0f7c34719b58261f660a3f1f5cdb4cd
Francisco <javiermartincuenca@gmail.com>
2024-06-04 17:01:57 +0200

    Setup background

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,13 @@
 export default function LoginPage() {
-  return <h1>Login</h1>;
+  return (
+    <div className='grid min-h-screen w-full grid-cols-[2fr_1fr]'>
+      <div className='bg-blue-400'></div>
+      <div className='flex flex-col justify-center items-center'>
+        <h2 className='text-3xl'>Login</h2>
+        <form>
+          <button>Login</button>
+        </form>
+      </div>
+    </div>
+  );
 }
