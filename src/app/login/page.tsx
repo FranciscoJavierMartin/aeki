acd16f267f1ed87937e88d5122c77569096a4515
Francisco <javiermartincuenca@gmail.com>
2024-06-05 09:50:33 +0200

    Add error message

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage() {
       <div className='bg-blue-400'></div>
       <div className='flex flex-col items-center justify-center'>
         <h2 className='text-3xl'>Login</h2>
-        <form className='flex w-[300px] flex-col gap-2 mt-4'>
+        <form className='mt-4 flex w-[300px] flex-col gap-2'>
           <InputForm id='username' name='username' placeholder='Username' />
           <InputForm
             id='password'
@@ -17,6 +17,9 @@ export default function LoginPage() {
           <button className='mt-6 w-full rounded-md bg-purple-500 px-3 py-2 text-white outline-none hover:bg-purple-600'>
             Login
           </button>
+          <p className='hidden text-sm text-red-400'>
+            This is an error message
+          </p>
         </form>
       </div>
     </div>
