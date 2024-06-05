baf59d9cfd70b223d1ae517494ea5402ed1eca2e
Francisco <javiermartincuenca@gmail.com>
2024-06-05 14:37:47 +0200

    Data table

--- src/app/(base)/customers/page.tsx
+++ src/app/(base)/customers/page.tsx
@@ -1,7 +1,48 @@
+const customers = [
+  {
+    name: 'John Doe',
+    dni: '12345678A',
+    email: 'john@doe.com',
+  },
+  {
+    name: 'Jane Doe',
+    dni: '12345678B',
+    email: 'john@doe.com',
+  },
+  {
+    name: 'Isaac Clarke',
+    dni: '12345678C',
+    email: 'isaac@clarke.com',
+  },
+  {
+    name: 'Agatha Christie',
+    dni: '12345678D',
+    email: 'agatha@christie.com',
+  },
+];
+
 export default function CustomersPage() {
   return (
     <div>
       <h1>Customers</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>DNI</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {customers.map((customer) => (
+            <tr key={customer.dni}>
+              <td>{customer.name}</td>
+              <td>{customer.dni}</td>
+              <td>{customer.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
