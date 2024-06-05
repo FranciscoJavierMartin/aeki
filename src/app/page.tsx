9f298c886bf43d5e1c579db365100efdd4742c51
Francisco <javiermartincuenca@gmail.com>
2024-06-05 12:28:46 +0200

    Adjust text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       <Header />
+      <div className='flex h-[calc(100vh-64px)] items-center justify-center'>
+        <h1>Hello world</h1>
+      </div>
     </div>
   );
 }
