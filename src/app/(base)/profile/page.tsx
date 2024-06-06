f9db0c2a16a42504422594ef28d76ffce6bc548c
Francisco <javiermartincuenca@gmail.com>
2024-06-06 10:27:50 +0200

    Address info

--- src/app/(base)/profile/page.tsx
+++ src/app/(base)/profile/page.tsx
@@ -10,46 +10,41 @@ export default async function ProfilePage() {
       <h2 className='text-3xl'>{user.name}&apos;s account</h2>
       <h4 className='text-2xl'>Personal info</h4>
       <div className='grid w-96 grid-cols-2 gap-y-2'>
-        <div>
-          <div>Name</div>
-          <div>{user.name}</div>
-        </div>
-        <div>
-          <div>Last name</div>
-          <div>{user.lastname}</div>
-        </div>
-        <div>
-          <div>Email</div>
-          <div>{user.emai}</div>
-        </div>
-        <div>
-          <div>DNI</div>
-          <div>{user.dni}</div>
-        </div>
-        <div>
-          <div>Phone</div>
-          <div>{user.phone}</div>
-        </div>
-        <div>
-          <div>Age</div>
-          <div>{diffYears(new Date(), new Date(user.dateOfBirth))}</div>
-        </div>
+        <div>Name</div>
+        <div>{user.name}</div>
+
+        <div>Last name</div>
+        <div>{user.lastname}</div>
+
+        <div>Email</div>
+        <div>{user.emai}</div>
+
+        <div>DNI</div>
+        <div>{user.dni}</div>
+
+        <div>Phone</div>
+        <div>{user.phone}</div>
+
+        <div>Age</div>
+        <div>{diffYears(new Date(), new Date(user.dateOfBirth))}</div>
       </div>
 
       <h4 className='text-2xl'>Address info</h4>
       <div className='grid w-96 grid-cols-2 gap-y-2'>
-        <div>
-          <div>Street</div>
-          <div>{user.address.street}</div>
-        </div>
-        <div>
-          <div>Town</div>
-          <div>{user.address.town}</div>
-        </div>
-        <div>
-          <div>Number</div>
-          <div>{user.address.no}</div>
-        </div>
+        <div>Street</div>
+        <div>{user.address.street}</div>
+
+        <div>Town</div>
+        <div>{user.address.town}</div>
+
+        <div>Number</div>
+        <div>{user.address.no}</div>
+
+        <div>ZIP</div>
+        <div>{user.address.zip}</div>
+
+        <div>State</div>
+        <div>{user.address.state}</div>
       </div>
     </div>
   );
