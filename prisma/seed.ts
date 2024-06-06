0a6c62ee8710c7933bab1997d22b1c2e31a6418a
Francisco <javiermartincuenca@gmail.com>
2024-06-06 16:08:02 +0200

    Seed customers

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -72,7 +72,25 @@ async function seedUsers(): Promise<void> {
   console.info('Creating users finished');
 }
 
-async function seedCustomers(): Promise<void> {}
+async function seedCustomers(): Promise<void> {
+  console.info('Creating customers');
+
+  await Promise.all(
+    new Array(5).fill(1).map(() =>
+      prisma.customer.create({
+        data: {
+          dni: (Math.random() * 100_000_000).toString().padStart(8, '0'),
+          email: faker.internet.email(),
+          firstName: faker.person.firstName(),
+          lastName: faker.person.lastName(),
+          phone: faker.phone.number(),
+        },
+      }),
+    ),
+  );
+
+  console.info('Creating customers finished');
+}
 
 async function seed() {
   await purgeData();
