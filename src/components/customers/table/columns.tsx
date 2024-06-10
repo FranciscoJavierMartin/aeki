e71b1df87a651761104a7c50efb2cf9116342f73
Francisco <javiermartincuenca@gmail.com>
2024-06-10 12:11:42 +0200

    Navigate to customer detail

--- src/components/customers/table/columns.tsx
+++ src/components/customers/table/columns.tsx
@@ -1,5 +1,16 @@
 'use client';
+import Link from 'next/link';
+import { DotsHorizontalIcon } from '@radix-ui/react-icons';
 import { ColumnDef } from '@tanstack/react-table';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 export const columns: ColumnDef<CustomerRow>[] = [
   {
@@ -17,5 +28,34 @@ export const columns: ColumnDef<CustomerRow>[] = [
   {
     accessorKey: 'email',
     header: 'Email',
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const customer = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <DotsHorizontalIcon className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(customer.dni)}
+            >
+              Copy customer DNI
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href={`/customers/${customer.dni}`}>View customer</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
   },
 ];
