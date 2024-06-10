9fb51e30ea117a203742e26fb81733f7a342d9fc
Francisco <javiermartincuenca@gmail.com>
2024-06-10 12:39:45 +0200

    Get selected data

--- src/app/api/customers/route.ts
+++ src/app/api/customers/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server';
 import { prismaClient } from '@/lib/utils/prisma-client';
 
 export async function GET(request: Request) {
-  const customers = await prismaClient.customer.findMany({});
+  const customers = await prismaClient.customer.findMany({
+    select: { dni: true, email: true, firstName: true, lastName: true },
+  });
   return NextResponse.json({ customers });
 }
