b24553861d79492caba9cd5e13ebd5fb5ae3044a
Francisco <javiermartincuenca@gmail.com>
2024-06-24 16:52:23 +0200

    Menu in mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { SheetTrigger } from './ui/sheet';
+import { Button } from './ui/button';
 
 const navLinks = [
   {
@@ -21,27 +23,29 @@ const navLinks = [
 
 export default function Header() {
   return (
-    <header className='flex w-full justify-between bg-slate-900'>
-      <button className='inline-block size-14 p-3 md:hidden'>
-        <span className='sr-only'>Open menu</span>
-        <svg
-          className='size-7 text-white'
-          aria-hidden='true'
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 17 14'
-        >
-          <path
-            stroke='currentColor'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth='2'
-            d='M1 1h15M1 7h15M1 13h15'
-          ></path>
-        </svg>
-      </button>
+    <header className='flex w-full flex-row-reverse items-center justify-between bg-slate-900 md:flex-row'>
+      <SheetTrigger asChild className='md:hidden'>
+        <Button variant='ghost'>
+          <span className='sr-only'>Open menu</span>
+          <svg
+            className='size-7 text-white'
+            aria-hidden='true'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 17 14'
+          >
+            <path
+              stroke='currentColor'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M1 1h15M1 7h15M1 13h15'
+            ></path>
+          </svg>
+        </Button>
+      </SheetTrigger>
       <span className='p-3 text-4xl text-white'>Aeki</span>
-      <nav className='flex justify-end text-right text-2xl transition-all duration-300 ease-in'>
+      <nav className='hidden justify-end text-right text-2xl transition-all duration-300 ease-in md:flex'>
         {navLinks.map((navLink) => (
           <Link
             href={navLink.href}
