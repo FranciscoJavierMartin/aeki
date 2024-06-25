562781899b7221371fb71fef1c37ba70ca9e45a2
Francisco <javiermartincuenca@gmail.com>
2024-06-25 13:23:42 +0200

    Tables in column

--- src/components/customers/detail/customer-details-page.tsx
+++ src/components/customers/detail/customer-details-page.tsx
@@ -36,7 +36,7 @@ export default function CustomerDetailsPage({ id }: { id: string }) {
   }));
 
   return (
-    <div className='flex flex-col gap-10 p-3'>
+    <div className='flex max-w-[100vw] flex-col gap-10 p-3'>
       <div className='my-10 flex w-full justify-center'>
         <div className='flex gap-5 rounded-2xl bg-slate-100 p-5'>
           <div className='flex size-40 items-center justify-center rounded-full bg-slate-900 text-8xl text-white'>
@@ -64,7 +64,7 @@ export default function CustomerDetailsPage({ id }: { id: string }) {
           </div>
         </div>
       </div>
-      <div className='grid grid-cols-2 gap-5'>
+      <div className='grid grid-cols-1 gap-5 md:grid-cols-2'>
         <DataTable data={products} columns={productColumns} />
         <DataTable data={budgets} columns={budgetColumns} />
       </div>
