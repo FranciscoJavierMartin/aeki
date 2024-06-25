308a4924cf4d9d2ecbd99b28dc1d7bff98da6d2d
Francisco <javiermartincuenca@gmail.com>
2024-06-25 16:33:18 +0200

    WIP

--- src/components/drawer-menu/drawer-menu.tsx
+++ src/components/drawer-menu/drawer-menu.tsx
@@ -31,31 +31,33 @@ const navLinks = [
 
 export default function Providers({ children }: PropsWithChildren) {
   return (
-    <Sheet>
-      {children}
-      <SheetContent side='left' className='bg-slate-900 text-white'>
-        <SheetHeader>
-          <SheetTitle className='text-4xl text-white'>
-            <SheetClose asChild>
-              <Link href='/'>Aeki</Link>
-            </SheetClose>
-          </SheetTitle>
-        </SheetHeader>
-        <div className='mt-5 flex flex-col gap-4 py-4 text-xl'>
-          {navLinks.map((navLink) => (
-            <SheetClose asChild key={navLink.href}>
-              <NavLink
-                href={navLink.href}
-                className='rounded-lg py-2 pl-4 pr-3 hover:bg-slate-700'
-                activeClass='bg-slate-700'
-              >
-                {navLink.text}
-              </NavLink>
-            </SheetClose>
-          ))}
-          <ThemeSwitcher />
-        </div>
-      </SheetContent>
-    </Sheet>
+    <div className='bg-blue-500 dark:bg-red-500'>
+      <Sheet>
+        {children}
+        <SheetContent side='left' className='bg-slate-900 text-white'>
+          <SheetHeader>
+            <SheetTitle className='text-4xl text-white'>
+              <SheetClose asChild>
+                <Link href='/'>Aeki</Link>
+              </SheetClose>
+            </SheetTitle>
+          </SheetHeader>
+          <div className='mt-5 flex flex-col gap-4 py-4 text-xl'>
+            {navLinks.map((navLink) => (
+              <SheetClose asChild key={navLink.href}>
+                <NavLink
+                  href={navLink.href}
+                  className='rounded-lg py-2 pl-4 pr-3 hover:bg-slate-700'
+                  activeClass='bg-slate-700'
+                >
+                  {navLink.text}
+                </NavLink>
+              </SheetClose>
+            ))}
+            <ThemeSwitcher />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </div>
   );
 }
