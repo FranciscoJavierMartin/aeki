b9ace3872624fbde4c133a3c603081949e5a6926
Francisco <javiermartincuenca@gmail.com>
2024-06-25 16:47:58 +0200

    Add change theme when load

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { getQueryClient } from '@/lib/utils/get-query-client';
@@ -7,6 +7,20 @@ import DrawerMenu from '@/components/drawer-menu/drawer-menu';
 
 export default function Providers({ children }: PropsWithChildren) {
   const queryClient = getQueryClient();
+
+  function setTheme(theme: string): void {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }
+
+  useEffect(() => {
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+      setTheme(theme);
+    } else {
+      setTheme('light');
+    }
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
