97de1747beb84ca195cec79fd993ce50def439e2
Francisco <javiermartincuenca@gmail.com>
2024-06-25 16:17:43 +0200

    Check handler

--- src/components/drawer-menu/theme-switcher.tsx
+++ src/components/drawer-menu/theme-switcher.tsx
@@ -1,10 +1,18 @@
+'use client';
+import { useState } from 'react';
 import { Label } from '@/components/ui/label';
 import { Switch } from '@/components/ui/switch';
 
 export default function ThemeSwitcher() {
+  const [isDark, setIsDark] = useState<boolean>(false);
+
   return (
     <div className='ml-2 flex items-center gap-x-4'>
-      <Switch id='airplane-mode' />
+      <Switch
+        id='airplane-mode'
+        checked={isDark}
+        onCheckedChange={() => setIsDark((prev) => !prev)}
+      />
       <Label htmlFor='airplane-mode' className='text-xl'>
         Dark mode
       </Label>
