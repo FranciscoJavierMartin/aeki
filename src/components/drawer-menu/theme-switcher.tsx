7652e99d1d934d71887297d23f4e76ff9b6b9838
Francisco <javiermartincuenca@gmail.com>
2024-06-25 16:13:50 +0200

    Remove attributes

--- src/components/drawer-menu/theme-switcher.tsx
+++ src/components/drawer-menu/theme-switcher.tsx
@@ -3,8 +3,8 @@ import { Switch } from '@/components/ui/switch';
 
 export default function ThemeSwitcher() {
   return (
-    <div className='flex items-center gap-x-4 ml-2'>
-      <Switch id='airplane-mode' className='text-red-400' color='#123456'/>
+    <div className='ml-2 flex items-center gap-x-4'>
+      <Switch id='airplane-mode' />
       <Label htmlFor='airplane-mode' className='text-xl'>
         Dark mode
       </Label>
