089ccdd9f0212247d7b438bd87416d753344f38b
Francisco <javiermartincuenca@gmail.com>
2024-06-27 17:15:19 +0200

    Route

--- src/app/api/customers/[id]/route.ts
+++ src/app/api/customers/[id]/route.ts
@@ -1,11 +1,17 @@
 import { NextResponse } from 'next/server';
 import { prismaClient } from '@/lib/utils/prisma-client';
+import { BudgetWithAmount } from '@/components/customers/detail/budget-columns';
+import { CustomerProductItem } from '@/components/customers/detail/product-columns';
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } },
 ) {
-  const customer = await prismaClient.customer.findUnique({
+  let products: CustomerProductItem[] = [];
+  let budgets: BudgetWithAmount[] = [];
+  let customer: Customer = {} as Customer;
+
+  const data = await prismaClient.customer.findUnique({
     where: { dni: params.id },
     include: {
       budgets: {
@@ -22,5 +28,37 @@ export async function GET(
       },
     },
   });
-  return NextResponse.json({ customer });
+
+  if (data) {
+    customer = {
+      dni: data.dni,
+      email: data.email,
+      firstName: data.firstName,
+      lastName: data.lastName,
+      id: data.id,
+      phone: data.phone,
+    };
+
+    products = data.budgets.reduce<CustomerProductItem[]>((acc, b) => {
+      b.products.forEach((product) => {
+        const index = acc.findIndex((p) => p.id === product.Product.id);
+
+        if (index > -1) {
+          acc[index].quantity += product.quantity;
+        } else {
+          acc.push({ ...product.Product, quantity: product.quantity });
+        }
+      });
+
+      return acc;
+    }, []);
+
+    budgets = data.budgets.map(({ dueDate, ...budget }) => ({
+      ...budget,
+      dueDate: dueDate.toDateString(),
+      amount: budget.products.length,
+    }));
+  }
+
+  return NextResponse.json({ customer, budgets, products });
 }
