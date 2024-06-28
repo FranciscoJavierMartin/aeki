8e5204593090aaa555fc045038d1dbaf4a8ecde8
Francisco <javiermartincuenca@gmail.com>
2024-06-28 12:46:37 +0200

    Add budget table

--- src/app/(base)/budgets/page.tsx
+++ src/app/(base)/budgets/page.tsx
@@ -1,6 +1,7 @@
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import { getQueryClient } from '@/lib/utils/get-query-client';
 import { budgetsOptions } from '@/components/budgets/options';
+import BudgetsTable from '@/components/budgets/table/budgets-table';
 
 export default function BudgetsPage() {
   const queryClient = getQueryClient();
@@ -12,8 +13,7 @@ export default function BudgetsPage() {
       <div className='container mx-auto py-10'>
         <h1 className='mb-5 text-center text-4xl'>Budgets</h1>
         <HydrationBoundary state={dehydrate(queryClient)}>
-          {/* <CustomersTable /> */}
-          <div>Hello</div>
+          <BudgetsTable />
         </HydrationBoundary>
       </div>
     </div>
---
+++ src/components/budgets/table/columns.tsx
@@ -0,0 +1,21 @@
+import { ColumnDef } from '@tanstack/react-table';
+import { BudgetRow } from '@/types/budget';
+import { Button } from '@/components/ui/button';
+import SortedIcon from '@/components/ui/sorted-icon';
+
+export const columns: ColumnDef<BudgetRow>[] = [
+  {
+    accessorKey: 'dueDate',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Due date
+          <SortedIcon isSorted={column.getIsSorted()} />
+        </Button>
+      );
+    },
+  },
+];
