874963d0f99a3e1e65787f8d9a5059c4229e51f8
Francisco <javiermartincuenca@gmail.com>
2024-07-02 13:37:26 +0200

    Product image

--- src/components/budgets/detail/product-column.tsx
+++ src/components/budgets/detail/product-column.tsx
@@ -1,9 +1,23 @@
 import { ColumnDef } from '@tanstack/react-table';
 import type { Product } from '@/types/budget';
+import { Avatar, AvatarImage } from '@/components/ui/avatar';
 
 export const productColumns: ColumnDef<Product>[] = [
   {
     accessorKey: 'photoURL',
+    header: '',
+    size: 30,
+    maxSize: 30,
+    minSize: 30,
+    cell: ({ row }) => {
+      return (
+        <Avatar>
+          <AvatarImage src={row.original.photoURL} />
+        </Avatar>
+      );
+    },
+    enableSorting: false,
+    enableHiding: false,
   },
   {
     accessorKey: 'name',
