aeac84a19004e9d1e2b7fabdd1251aeb291b47a7
Francisco <javiermartincuenca@gmail.com>
2024-07-02 17:22:02 +0200

    Budget data

--- src/components/budgets/detail/budget-details-page.tsx
+++ src/components/budgets/detail/budget-details-page.tsx
@@ -9,6 +9,28 @@ export default function BudgetDetailsPage({ id }: { id: string }) {
 
   return (
     <div>
+      <div className='flex w-full gap-x-5'>
+        <section className='grid w-1/2 grid-cols-[2fr_4fr] gap-x-4 gap-y-3 rounded-3xl bg-slate-100 p-5 dark:bg-slate-800'>
+          <span className='font-semibold'>ID</span>
+          <span className='text-right'>{data.budget.id}</span>
+          <span className='font-semibold'>Due date</span>
+          <span className='text-right'>
+            {new Date(data.budget.dueDate).toLocaleDateString()}
+          </span>
+          <span className='font-semibold'>Total price</span>
+          <span className='text-right'>
+            {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD',
+            }).format(data.budget.totalPrice)}
+          </span>
+          <span className='font-semibold'>Total price</span>
+          <span className='text-right'>
+            {data.budget.discountAppliedPercentage}%
+          </span>
+        </section>
+      </div>
+      <pre>{JSON.stringify({ ...data.customer }, null, 2)}</pre>
       <div className='mb-30 w-full px-3'>
         <DataTable data={data.products} columns={productColumns} />
       </div>
