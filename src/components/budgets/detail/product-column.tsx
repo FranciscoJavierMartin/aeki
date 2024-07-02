62bf4a4bd648a21755acfed6b3ecb497d99bba81
Francisco <javiermartincuenca@gmail.com>
2024-07-02 13:53:29 +0200

    Name, quantity and price per unit

--- src/components/budgets/detail/product-column.tsx
+++ src/components/budgets/detail/product-column.tsx
@@ -1,6 +1,8 @@
 import { ColumnDef } from '@tanstack/react-table';
 import type { Product } from '@/types/budget';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import SortedIcon from '@/components/ui/sorted-icon';
 
 export const productColumns: ColumnDef<Product>[] = [
   {
@@ -21,11 +23,61 @@ export const productColumns: ColumnDef<Product>[] = [
   },
   {
     accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant='ghost'
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Name
+          <SortedIcon isSorted={column.getIsSorted()} />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: 'quantity',
+    header: ({ column }) => {
+      return (
+        <div className='text-right'>
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          >
+            Quantity
+            <SortedIcon isSorted={column.getIsSorted()} />
+          </Button>
+        </div>
+      );
+    },
+    cell: ({ row }) => {
+      const quantity = parseFloat(row.getValue('quantity'));
+      return <div className='text-center md:text-right'>{quantity}</div>;
+    },
   },
   {
     accessorKey: 'pricePerUnit',
+    header: ({ column }) => {
+      return (
+        <div className='text-right'>
+          <Button
+            variant='ghost'
+            onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+          >
+            Price per unit
+            <SortedIcon isSorted={column.getIsSorted()} />
+          </Button>
+        </div>
+      );
+    },
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue('pricePerUnit'));
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount);
+
+      return <div className='text-right'>{formatted}</div>;
+    },
   },
 ];
