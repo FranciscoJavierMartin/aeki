64049de6ac20df4273c77b7eb4ae9a00cb55000f
Francisco <javiermartincuenca@gmail.com>
2024-07-03 11:17:54 +0200

    Move inside table

--- src/components/budgets/detail/budget-details-page.tsx
+++ src/components/budgets/detail/budget-details-page.tsx
@@ -29,16 +29,23 @@ export default function BudgetDetailsPage({ id }: { id: string }) {
             <span className='text-right'>
               {data.budget.discountAppliedPercentage}%
             </span>
-          </section>
-        )}
-        {data.customer && (
-          <section className='grid w-1/2 grid-cols-[2fr_4fr] gap-x-4 gap-y-3 rounded-3xl bg-slate-100 p-5 dark:bg-slate-800'>
-            <span>Customer</span>
-            <span>{`${data.customer.lastName}, ${data.customer.firstName}`}</span>
-            <span>Email</span>
-            <a href={`mailto:${data.customer.email}`}>{data.customer.email}</a>
-            <span>Phone</span>
-            <a href={`tel:${data.customer.phone}`}>{data.customer.phone}</a>
+            {data.customer && (
+              <>
+                <span className='font-semibold'>Customer</span>
+                <span className='text-right'>{`${data.customer.lastName}, ${data.customer.firstName}`}</span>
+                <span className='font-semibold'>Email</span>
+                <a
+                  href={`mailto:${data.customer.email}`}
+                  className='text-right'
+                >
+                  {data.customer.email}
+                </a>
+                <span className='font-semibold'>Phone</span>
+                <a href={`tel:${data.customer.phone}`} className='text-right'>
+                  {data.customer.phone}
+                </a>
+              </>
+            )}
           </section>
         )}
       </div>
